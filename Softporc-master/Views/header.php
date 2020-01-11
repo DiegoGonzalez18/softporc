ba8a752475ceefe630c35ff040c9142bb199a62f
DIEGO ALIRIO GONZALEZ MELGAREJO <diegoaliriogm@ufps.edu.co>
2020-01-11 12:52:33 -0500

    codigo

---
+++ Softporc-master/Views/header.php
@@ -0,0 +1,26 @@
+<?php
+
+print('<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Softporc">
+  <meta name="author" content="Isoftporc.com">
+  <meta name="keyword" content="Softporc">
+
+  <title>Sistema Ventas - IncanatoIT</title>
+
+  <!-- Icons -->
+  <link href="./Public/css/simple-line-icons.min.css" rel="stylesheet">
+  <link href="./Public/css/font-awesome.min.css" rel="stylesheet">
+  
+  <!-- Main styles for this application -->
+  <link href="./Public/css/style.css" rel="stylesheet">
+
+</head>
+
+<body >');
